3fa076697e42fc8366d57eabefc357eb050e10c6
ArkJzzz <arkjzzz@gmail.com>
2016-11-14 00:18:16 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
