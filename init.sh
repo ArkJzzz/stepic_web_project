9ee3d44194cd2d4ed150d7b47316906cabdcaf25
ArkJzzz <arkjzzz@gmail.com>
2017-02-23 20:21:29 +0000

    step 2.1.11-a

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
-sudo rm -f /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
+
+sudo rm -f /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
 sudo ln -s /home/box/web/etc/django-gunicorn.config /etc/gunicorn.d/django.config
 sudo /etc/init.d/gunicorn restart
