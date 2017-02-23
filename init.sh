adea36ac577bc5a6b1b772266f8880a2c7ac4e99
ArkJzzz <arkjzzz@gmail.com>
2017-02-23 19:40:36 +0000

    step 2.1.11

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
+sudo ln -s /home/box/web/etc/django-gunicorn.config /etc/gunicorn.d/django.config
+sudo /etc/init.d/gunicorn restart
--- rpi-init.sh
+++ rpi-init.sh
@@ -4,7 +4,7 @@ echo "Configure Nginx:"
 echo "removing old configs from /etc/nginx/sites-enabled/"
 sudo rm -f /etc/nginx/sites-enabled/{default,test.conf}
 echo "creating simlinks"
-sudo ln -s /home/pi/stepic_web_project/web/etc/nginx-rpi.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/pi/stepic_web_project/web/etc/rpi-nginx.conf  /etc/nginx/sites-enabled/test.conf
 
 echo "Configure GUNICORN:"
 echo "removing old configurations from /etc/gunicorn.d/"
@@ -12,14 +12,16 @@ sudo rm -f /etc/gunicorn.d/{hello*,ask*}
 sudo mkdir -p /etc/gunicorn.d/backup_configs
 sudo mv -f /etc/gunicorn.d/* /etc/gunicorn.d/backup_configs
 echo "creating simlinks"
-sudo ln -s /home/pi/stepic_web_project/etc/hello-rpi-gunicorn.config /etc/gunicorn.d/hello.config
-#sudo ln -s /home/pi/stepic_web_project/etc/ask-rpi-gunicorn_config.py /etc/gunicorn.d/ask.config
+sudo ln -s /home/pi/stepic_web_project/etc/rpi-hello-gunicorn.config /etc/gunicorn.d/hello.config
+sudo ln -s /home/pi/stepic_web_project/etc/rpi-django-gunicorn.config /etc/gunicorn.d/django.config
 
 echo "restarting Nginx"
 sudo /etc/init.d/nginx restart
 
 echo "restarting GUNICORN"
 sudo /etc/init.d/gunicorn restart
+
+
 
 #
 
