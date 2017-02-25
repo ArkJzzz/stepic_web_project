3d8afe0daa4d21c877b1b2f6933671b15ba2b71b
ArkJzzz <arkjzzz@gmail.com>
2017-02-25 08:41:04 +0000

    mv from stepic_web_project dir all files to home/box dir

--- init.sh
+++ init.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 
-mkdir -p ~/web/{public/{img,js,css},{etc,uploads}}
+cd /home/box/
+sudo mv /home/box/stepic_web_project/* /home/box/
+rm -r /home/box/stepic_web_project/
+
+mkdir -p /home/box/web/{public/{img,js,css},{etc,uploads}}
 sudo rm -f /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 
-sudo rm -f /etc/gunicorn.d/*
+#sudo rm -f /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
 sudo ln -s /home/box/web/etc/django-gunicorn.config /etc/gunicorn.d/django.config
 sudo /etc/init.d/gunicorn restart
+
