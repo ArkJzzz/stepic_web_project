4a02d8509ff692ee63ec7c70b25d850bf221fed6
ArkJzzz <arkjzzz@gmail.com>
2017-02-26 20:57:02 +0300

    step 2.1.11 done

--- init.sh
+++ init.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+BASE_DIR=/home/box
 
-cd /home/box/
-sudo mv /home/box/stepic_web_project/* /home/box/
-rm -r /home/box/stepic_web_project/
+cd ${BASE_DIR}/
+sudo mv ${BASE_DIR}/stepic_web_project/* ${BASE_DIR}/
+rm -r ${BASE_DIR}/stepic_web_project/
 
-mkdir -p /home/box/web/{public/{img,js,css},{etc,uploads}}
+mkdir -p ${BASE_DIR}/web/{public/{img,js,css},{etc,uploads}}
 sudo rm -f /etc/nginx/sites-enabled/*
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ${BASE_DIR}/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 
 #sudo rm -f /etc/gunicorn.d/*
-sudo ln -s /home/box/web/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
-sudo ln -s /home/box/web/etc/django-gunicorn.config /etc/gunicorn.d/django.config
-sudo /etc/init.d/gunicorn restart
+sudo ln -s ${BASE_DIR}/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
+sudo ln -s ${BASE_DIR}/etc/django-gunicorn.config /etc/gunicorn.d/django.config
+sudo /etc/init.d/gunicorn restart	
+
 
--- rpi-init.sh
+++ rpi-init.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+BASE_DIR=/home/pi/stepic_web_project
+
 echo "Configure Nginx:"
 echo "removing old configs from /etc/nginx/sites-enabled/"
 sudo rm -f /etc/nginx/sites-enabled/{default,test.conf}
 echo "creating simlinks"
-sudo ln -s /home/pi/stepic_web_project/web/etc/rpi-nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ${BASE_DIR}/web/etc/rpi-nginx.conf  /etc/nginx/sites-enabled/test.conf
 
 echo "Configure GUNICORN:"
 echo "removing old configurations from /etc/gunicorn.d/"
 sudo rm -f /etc/gunicorn.d/{hello*,ask*}
-sudo mkdir -p /etc/gunicorn.d/backup_configs
-sudo mv -f /etc/gunicorn.d/* /etc/gunicorn.d/backup_configs
+#sudo mkdir -p /etc/gunicorn.d/backup_configs
+#sudo mv -f /etc/gunicorn.d/* /etc/gunicorn.d/backup_configs
 echo "creating simlinks"
-sudo ln -s /home/pi/stepic_web_project/etc/rpi-hello-gunicorn.config /etc/gunicorn.d/hello.config
-sudo ln -s /home/pi/stepic_web_project/etc/rpi-django-gunicorn.config /etc/gunicorn.d/django.config
+sudo ln -s ${BASE_DIR}/etc/rpi-hello-gunicorn.config /etc/gunicorn.d/hello.config
+sudo ln -s ${BASE_DIR}/etc/rpi-django-gunicorn.config /etc/gunicorn.d/django.config
 
 echo "restarting Nginx"
 sudo /etc/init.d/nginx restart
