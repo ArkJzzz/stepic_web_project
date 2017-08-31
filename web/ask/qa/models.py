2637a2fca50f4cf42352851f565e62792865c658
ArkJzzz <arkjzzz@gmail.com>
2017-08-31 13:27:31 +0300

    change web/ask/qa/models.py

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from djago.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_lengt=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	likes = models.IntegerField(default=0)
+
+class QuestionManager(models.Manager):
+	"""менеджер модели Question"""
+	# метод возвращающий последние добавленные вопросы
+	def new(self):
+		return self.order_by('-added_at')
+    # метод возвращающий вопросы отсортированные по рейтингу 
+	def popular(self):
+		return self.order_by('-rating')
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(blank=True)
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User)
+
+
+
