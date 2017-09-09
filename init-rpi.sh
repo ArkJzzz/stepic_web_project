ccca8787bea2504a271b9a50dbe991221b818a37
ArkJzzz <arkjzzz@gmail.com>
2017-09-09 21:41:06 +0300

    init-rpi.sh

--- init-rpi.sh
+++ init-rpi.sh
@@ -8,13 +8,13 @@ mkdir -p ${BASE_DIR}/web/{public/{img,js,css},{etc,uploads}}
 
 # NGINX
 sudo rm -f /etc/nginx/sites-enabled/*
-sudo ln -s ${BASE_DIR}/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ${BASE_DIR}/web/etc/rpi-nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 # GUNICORN
 sudo rm -f /etc/gunicorn.d/*
-sudo ln -s ${BASE_DIR}/etc/hello-gunicorn.config /etc/gunicorn.d/hello.config
-sudo ln -s ${BASE_DIR}/etc/django-gunicorn.config /etc/gunicorn.d/django.config
+sudo ln -s ${BASE_DIR}/etc/rpi-hello-gunicorn.config /etc/gunicorn.d/hello.config
+sudo ln -s ${BASE_DIR}/etc/rpi-django-gunicorn.config /etc/gunicorn.d/django.config
 sudo /etc/init.d/gunicorn restart
 
 #MySQL
