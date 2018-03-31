4e67d1178dc8eb718b5d390770f239dae05552e6
ArkJzzz <arkjzzz@gmail.com>
2018-03-31 22:28:09 +0300

    решение задачи 2.5.8

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 # Create your models here.
 class QuestionManager(models.Manager):
 	def new(self):
-		return self.order_by('-added_at')
+		return self.order_by('-id')
 	def popular(self):
 		return self.order_by('-rating')
 
